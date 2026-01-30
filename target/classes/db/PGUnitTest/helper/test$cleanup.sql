039ed492f5cc7653062e8550a601e6257ebafba4
fannykdl <fanny@kdl.co.jp>
2026-01-30 17:00:16 +0900

    ビルド成果物（target/）を管理対象から除外

--- target/classes/db/PGUnitTest/helper/test$cleanup.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE PROCEDURE test.test$cleanup() LANGUAGE plpgsql as $$
-BEGIN
-
-	TRUNCATE TABLE test_results;
-END;
-$$;
--- target/classes/db/PGUnitTest/src/demo_table.sql
+++
@@ -1,2 +0,0 @@
-create table demo_table (id int primary key, name varchar(20));
-insert into demo_table values (1,'test1'),(2,'test2');
